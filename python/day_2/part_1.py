0688206a7a893d3f3d34ba0ce704787d20e17801
ddoyle <ddoyle@synopsys.com>
2023-12-04 22:53:41 +0000

    added day 2 part 1 python

---
+++ python/day_2/part_1.py
@@ -0,0 +1,36 @@
+
+
+with open("day_2/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+
+def is_too_high(color, number):
+    if color == "red" and number > 12:
+        return True
+    if color == "green" and number > 13:
+        return True
+    if color == "blue" and number > 14:
+        return True
+    return False
+
+total_possible = 0
+for line in lines:
+    gamenumber, fullgame = line.split(":")
+    g_number = int(''.join([i for i in gamenumber if i.isdigit()]).strip(' '))
+    cube_games = fullgame.split(";")
+    for game in cube_games:
+        possible = False
+        cubes = game.split(",")        
+        for hand in cubes:
+            color = ''.join([i for i in hand if not i.isdigit()]).strip(' ')
+            number = int(''.join([i for i in hand if i.isdigit()]).strip(' '))
+            if is_too_high(color, number):
+                g_number = 0
+                break
+            else:
+                possible = True
+    if possible:
+        total_possible = total_possible + g_number
+
+print(total_possible)
+
